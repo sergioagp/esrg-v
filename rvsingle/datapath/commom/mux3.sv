b36d1f9bbd834d88cb80b100713c8d3588c4bbc1
sergioagp <sergiogspereira@gmail.com>
2022-04-28 12:33:47 +0100

    update mux3

--- rvsingle/datapath/commom/mux3.sv
+++ rvsingle/datapath/commom/mux3.sv
@@ -1,6 +1,6 @@
 module mux3 #(parameter WIDTH = 8)(
     input logic [WIDTH-1:0]     d0, d1, d2,
-    input logic [2:0]           s,
+    input logic [1:0]           s,
     output logic [WIDTH-1:0]    y
 );
 
