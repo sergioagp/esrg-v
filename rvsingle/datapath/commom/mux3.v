b5267e7510aa3275a03fa6ba59834680694d8c2c
sergioagp <sergiogspereira@gmail.com>
2022-04-28 12:35:21 +0100

    update mux3

--- rvsingle/datapath/commom/mux3.v
+++ rvsingle/datapath/commom/mux3.v
@@ -9,7 +9,7 @@ module mux3 (
 	input wire [WIDTH - 1:0] d0;
 	input wire [WIDTH - 1:0] d1;
 	input wire [WIDTH - 1:0] d2;
-	input wire s[1:0];
+	input wire [1:0] s;
 	output wire [WIDTH - 1:0] y;
 	assign y = (s[1] ? d2 : (s[0] ? d1 : d0));
 endmodule
